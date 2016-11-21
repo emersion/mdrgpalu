75aef2af9e2c36746c95174354d6eaea648f8705
emersion <contact@emersion.fr>
2016-11-21 18:00:22 +0100

    node -> trie_node

--- src/autocomplete.c
+++ src/autocomplete.c
@@ -1,27 +1,27 @@
-struct autocomplete_node {
-	struct node* next;
-	struct node* first;
+struct trie_node {
+	struct trie_node* next;
+	struct trie_node* first;
 	char ch;
 };
 
-struct node* node_new() {
-	struct node* n = (struct node*) malloc(sizeof(struct node));
+struct trie_node* trie_node_new() {
+	struct trie_node* n = (struct trie_node*) malloc(sizeof(struct trie_node));
 	n->next = NULL;
 	n->first = NULL;
 	n->ch = 0;
 	return n;
 }
 
-void node_free(struct node* n) {
+void trie_node_free(struct trie_node* n) {
 	free(n);
 }
 
-struct node* node_match(struct node* first, char* query, int len) {
+struct trie_node* trie_node_match(struct trie_node* first, char* query, int len) {
 	if (len == 0) {
 		return NULL;
 	}
 
-	struct node* n = first;
+	struct trie_node* n = first;
 	while (n != NULL && n->ch < query[0]) {
 		n = n->next;
 	}
@@ -32,18 +32,18 @@ struct node* node_match(struct node* first, char* query, int len) {
 	if (n->first == NULL) {
 		return NULL;
 	}
-	return node_match(n->first, &query[1], len-1);
+	return trie_node_match(n->first, &query[1], len-1);
 }
 
-void node_insert(struct node* first, struct node* new) {
-	struct node* n = first;
+void trie_node_insert(struct trie_node* first, struct trie_node* new) {
+	struct trie_node* n = first;
 	while (n->ch < new->ch && n->next != NULL) {
 		n = n->next;
 	}
 
 	if (n->ch == new->ch) {
 		if (n->first != NULL && new->first != NULL) {
-			node_insert(n->first, new->first);
+			trie_node_insert(n->first, new->first);
 		} else if (n->first == NULL) {
 			n->first = new->first;
 		}
